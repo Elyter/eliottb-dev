161459244299aa3fd8ef5009e6a3c38096c74c59
Elyter <eliottbreg@gmail.com>
2025-01-09 16:35:59 +0100

    fix error

--- src/components/download-button.tsx
+++ src/components/download-button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Star = () => (
   <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" version="1.1" 
-       style={{shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd'}} 
+       style={{shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: '-webkit-optimize-contrast', fillRule: 'evenodd', clipRule: 'evenodd'}} 
        viewBox="0 0 784.11 815.53">
     <path className="fill-[#fec195] dark:fill-white" d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" />
   </svg>
